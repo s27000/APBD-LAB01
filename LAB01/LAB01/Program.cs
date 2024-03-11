69665f4f8871c799fe4ed646a9a53ad076c756dc
Adam Bartkiewicz <s27000@pjwstk.edu.pl>
2024-03-11 13:16:38 +0100

    LAB01

--- LAB01/LAB01/Program.cs
+++ LAB01/LAB01/Program.cs
@@ -1,8 +1,8 @@
 ﻿namespace LAB01
 {
-    internal class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
         }
