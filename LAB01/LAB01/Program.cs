69616b0968389a93e5aabbd8ffe084d4e1e399eb
Adam Bartkiewicz <s27000@pjwstk.edu.pl>
2024-03-11 13:23:58 +0100

    LAB01

--- LAB01/LAB01/Program.cs
+++ LAB01/LAB01/Program.cs
@@ -4,7 +4,8 @@
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Start");
+            Console.WriteLine("Zadanie 1");
         }
     }
 }
